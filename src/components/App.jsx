67d5cce7b284bbcb0a0bec47596af14ee2c91264
Lily Anderson <thatoanderson92@gmail.com>
2025-12-30 22:35:14 -0500

    install lucide-react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Offers from "./Offers";
+import StatsCta from "./StatsCta";
 
-export default App;
+export default function App() {
+  return (
+    <div className="min-h-screen bg-cream">
+      <Navbar />
+      <main className="px-4">
+        <Hero />
+        <Offers />
+        <StatsCta />
+      </main>
+    </div>
+  );
+}
